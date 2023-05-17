f7da10ab9af020bed6d7b0d7091d3965c7a50039
JuanDecoud <juanjo.decoud@gmail.com>
2023-05-17 12:05:25 -0300

    asd

--- app.js
+++ app.js
@@ -13,19 +13,13 @@ app.get (`/Products` ,async (req , res)=>{
     let products = await productManager.getProducts()
     if(limit>0){
         const productsWlimit = []
-        for (let index = 0; index <= limit-1; index++) {
-             productsWlimit.push (products[index]);   
-        }
+        for (let index = 0; index <= limit-1; index++) productsWlimit.push (products[index]);   
         res.send(productsWlimit)
     }
     if (pid!=undefined){
         let searchProduct = products.find(element=> element.id == pid)
-        console.log(searchProduct)
-        if (searchProduct ===undefined){
-            res.send (`no se encuentra el producto`)
-        }else {
-            res.json (searchProduct)
-        }
+        if (searchProduct ===undefined)res.send (`no se encuentra el producto`)
+        else res.json (searchProduct)
     }
     if (limit === undefined && pid === undefined)res.send(products)
 })
